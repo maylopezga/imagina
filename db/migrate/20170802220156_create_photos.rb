069a66a455023b916c8b98199564277478a6f250
maylopezga <maye.lopez16@gmail.com>
2017-08-02 17:02:46 -0500

    Photos

---
+++ db/migrate/20170802220156_create_photos.rb
@@ -0,0 +1,11 @@
+class CreatePhotos < ActiveRecord::Migration[5.1]
+  def change
+    create_table :photos do |t|
+      t.references :user, foreign_key: true
+      t.string :title
+      t.text :body
+
+      t.timestamps
+    end
+  end
+end
